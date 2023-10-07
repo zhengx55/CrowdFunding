bea234fdffb884e7901a9be43c8be0d4705af4b4
zhengx55 <williamzhxin@gmail.com>
2023-10-07 15:05:31 -0400

    client setup

---
+++ client/pages/index.tsx
@@ -0,0 +1,11 @@
+import { ConnectWallet } from "@thirdweb-dev/react";
+import Image from "next/image";
+import { NextPage } from "next";
+
+const Home: NextPage = () => {
+  return (
+  <main></main>
+  );
+};
+
+export default Home;
